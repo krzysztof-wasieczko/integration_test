89b62293c858929d6dd63d3816b00b0d146fb597
krzysztof-wasieczko <147060860+krzysztof-wasieczko@users.noreply.github.com>
2023-10-05 15:21:00 +0200

    Added new line to main.cpp
    This is the first commit after setting GitLab mirroring

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main()
 {
     std::cout << "Hello World" << std::endl;
+    std::cout << "Now it's time to next step: this line was added after GitLab mirroring!" << std::endl;
     return 0;
 }
