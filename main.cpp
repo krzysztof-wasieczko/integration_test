0ef206fc22325fd568d380befcb35445c5024c12
krzysztof-wasieczko <147060860+krzysztof-wasieczko@users.noreply.github.com>
2023-10-06 09:57:36 +0200

    Message no. 3
    Additional line

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main()
 {
     std::cout << "Hello World" << std::endl;
     std::cout << "Now it's time to next step: this line was added after GitLab mirroring!" << std::endl;
+    std::cout << "Message no.3 added" << std::endl;
     return 0;
 }
