fa704e6702f4f82fb434468f170eb11ed9512341
krzysztof-wasieczko <147060860+krzysztof-wasieczko@users.noreply.github.com>
2023-10-20 09:18:08 +0200

    New branch on GitLab

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello World" << std::endl;
     std::cout << "Now it's time to next step: this line was added after GitLab mirroring!" << std::endl;
     std::cout << "Message no.3 added" << std::endl;
+    std::cout << "We have a new branch on Gitlab!" << std::endl;
     return 0;
 }
